16062c5e4e88c43be41dabf5cf6f30731af097f2
Roy Bellingan <roy@tech.techadsmedia.com>
2021-01-30 14:22:34 +0000

    fmt ed errori

--- magic_from_string.hpp
+++ magic_from_string.hpp
@@ -1,9 +1,31 @@
+#include "fmt/format.h"
 #include "magic_enum.hpp"
 #include <QByteArray>
+#include <QStacker/qstacker.h>
 #include <QString>
 #include <concepts>
 
 namespace magic_enum {
+template <typename Type>
+std::string join(const Type& t) {
+	std::string final;
+	auto        size = t.size();
+	for (uint i = 0; i < size; i++) {
+		final.append(t[i]);
+
+		if (i != size - 1)
+			final.append(" - ");
+	}
+	return final;
+}
+
+template <typename Type>
+std::string composeError(const std::string& key, Type en) {
+	auto& names      = magic_enum::enum_names<Type>();
+	auto  nameString = join(names);
+	return fmt::format("The key >>>{}<<< is not contained in the enum >>>{}<<<", key, nameString);
+}
+
 template <typename T, typename K>
 [[nodiscard]] T fromString(const K& string) {
 	return enum_cast<T>(string.toStdString()).value();
@@ -12,6 +34,17 @@ template <typename T, typename K>
 template <typename T, typename K>
 [[nodiscard]] T fromString(const std::string& string) {
 	return enum_cast<T>(string).value();
+}
+
+template <typename T>
+void fromString(const std::string& string, T& t) {
+	auto opt = enum_cast<T>(string);
+	if (opt.has_value()) {
+		t = opt.value();
+	} else {
+		auto  msg         = composeError(string, t);
+		throw ExceptionV2(msg);
+	}
 }
 
 template <typename E>
